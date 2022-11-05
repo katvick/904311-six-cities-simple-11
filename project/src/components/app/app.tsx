78da130617b0aa7e278bfb237b38ff25a6257cb6
Kate <stolyarova.katy@ya.ru>
2022-11-05 16:18:04 +0500

    добавление маршрутизации

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,34 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
+
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import RoomPage from '../../pages/room-page/room-page';
+
 
 type AppProps = {
   countRentOffers: number;
 }
 
 function App({countRentOffers}: AppProps): JSX.Element {
-  return <MainPage countRentOffers={countRentOffers}/>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage countRentOffers={countRentOffers} />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<RoomPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
