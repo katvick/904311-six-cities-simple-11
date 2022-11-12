0c96396c9c7467002102881c3227fd7a47c1e316
Kate <stolyarova.katy@ya.ru>
2022-11-12 22:30:01 +0500

    добавление в состояние активной OfferCard

--- project/src/components/list-offers/list-offers.tsx
+++ project/src/components/list-offers/list-offers.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import OfferCard from '../offer-card/offer-card';
 import { Offers } from '../../types/mocks';
 
@@ -6,14 +7,16 @@ type ListOffersProps = {
 }
 
 function ListOffers({offers}: ListOffersProps): JSX.Element {
+  const [ , setOfferMouseOver] = useState(offers[0]);
+
   return (
     <div className="cities__places-list places__list tabs__content">
       {offers.map((offer) => {
         const keyValue = `${offer.id}-${offer.header}`;
         return (
-          <article key={keyValue} className="cities__card place-card" onFocus={() => {
-            // setOfferOnFocus(offer.id);
-            // // console.log(offerOnFocus);
+          <article key={keyValue} className="cities__card place-card" onMouseOver={() => {
+            setOfferMouseOver(offer);
+            // console.log(offerMouseOver);
           }}
           >
             <OfferCard offer={offer} />
