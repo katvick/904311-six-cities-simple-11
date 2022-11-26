3e0bb1f6a380cb266b5ab4736eeb88373c903b56
Kate <stolyarova.katy@ya.ru>
2022-11-26 15:59:38 +0500

    инициализирует новое хранилище

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
 import { city } from './mocks/city';
+import { store } from './store';
 
 const Setting = {
   CountRentOffers: 10
@@ -14,10 +16,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      countRentOffers = {Setting.CountRentOffers}
-      offers = {offers}
-      city = {city}
-    />
+    <Provider store = {store}>
+      <App
+        countRentOffers = {Setting.CountRentOffers}
+        offers = {offers}
+        city = {city}
+      />
+    </Provider>
   </React.StrictMode>,
 );
