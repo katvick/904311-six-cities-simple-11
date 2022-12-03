827cc64fd334829ec89eb601dd662589a67f58d2
Kate <stolyarova.katy@ya.ru>
2022-12-03 21:15:48 +0500

    реализует оранжевую подсветку активного маркера на карте

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -15,13 +15,13 @@ type MapProps = {
 
 const defaultIcon = new Icon({
   iconUrl: URL_MARKER_DEFAULT,
-  iconSize: [40, 40],
+  iconSize: [27, 39],
   iconAnchor: [20, 40],
 });
 
 const currentIcon = new Icon({
   iconUrl: URL_MARKER_CURRENT,
-  iconSize: [40, 40],
+  iconSize: [27, 39],
   iconAnchor: [20, 40],
 });
 
