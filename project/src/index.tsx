804ee78c36db4368b52263fcf2a9ece172148165
Kate <stolyarova.katy@ya.ru>
2022-12-08 14:09:19 +0500

    добавит обработку ошибок взаимодействия с сервером

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -4,6 +4,9 @@ import { Provider } from 'react-redux';
 import App from './components/app/app';
 import ErrorMesage from './components/error-mesage/error-mesage';
 import { store } from './store';
+import { checkAuthAction } from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
