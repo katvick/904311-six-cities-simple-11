8796bebecc8fd5d9c99b93288b84f7b31919eca4
Kate <stolyarova.katy@ya.ru>
2022-12-18 23:55:45 +0500

    реализует закрытие блока сортировки

--- project/src/components/sorting/sorting.tsx
+++ project/src/components/sorting/sorting.tsx
@@ -1,32 +1,40 @@
 import { useAppDispatch } from '../../hooks';
 import { SortType } from '../../const';
 import { changeSort, sortOffers } from '../../store/offers/offers';
+import { useState } from 'react';
 
 type SortingProps = {
   sortType: string;
 }
 
 function Sorting({sortType}: SortingProps): JSX.Element {
-  const dispatch = useAppDispatch();
+  const [toggleList, setToggleList] = useState<boolean>(false);
 
-  const changeSortHandle = (evt: React.MouseEvent<HTMLUListElement>) => {
-    const target = evt.target as HTMLLIElement;
-    dispatch(changeSort({sort: target.innerText}));
-    dispatch(sortOffers());
-  };
+  const dispatch = useAppDispatch();
 
   return (
     <form className="places__sorting" action="/" method="get">
       <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0}>
+      <span className="places__sorting-type" tabIndex={0} onClick={() => setToggleList((prevState) => !prevState)}>
         {sortType}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="/icon-arrow-select"></use>
         </svg>
       </span>
-      <ul className="places__options places__options--custom places__options--opened" onClick={changeSortHandle}>
+      <ul className={`places__options places__options--custom ${toggleList ? 'places__options--opened' : ''}`}>
         {Object.entries(SortType).map(([key, value]) => (
-          <li key={key} className={`places__option ${sortType === value ? 'places__option--active' : ''}`} tabIndex={0}>{value}</li>
+          <li
+            key={key}
+            className={`places__option ${sortType === value ? 'places__option--active' : ''}`}
+            tabIndex={0}
+            onClick={() => {
+              setToggleList(false);
+              dispatch(changeSort({sort: value}));
+              dispatch(sortOffers());
+            }}
+          >
+            {value}
+          </li>
         ))}
       </ul>
     </form>
